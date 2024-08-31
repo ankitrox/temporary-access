383a1aa4fa1d82743dd68276b89a95cd83cd74b2
Ankit Gade <ankit.gade@10up.com>
2024-08-31 16:45:28 +0530

    Tests: edit form render.

--- assets/js/components/EditForm/index.test.js
+++ assets/js/components/EditForm/index.test.js
@@ -5,6 +5,7 @@
 import { createTestRegistry, render } from 'test-utils';
 import EditForm from './index';
 import { UI_STORE_NAME } from '../../datastores/constants';
+import { prettyDOM } from '@testing-library/dom';
 
 describe('EditForm', () => {
 	let registry;
@@ -13,45 +14,59 @@ describe('EditForm', () => {
 		registry = createTestRegistry();
 	});
 
-	it('should render null when the context is not edit', async () => {
-		registry.dispatch(UI_STORE_NAME).setStep(2);
-		registry.dispatch(UI_STORE_NAME).setContext('default');
+	describe('returns null', () => {
+		it('when the context is not edit', async () => {
+			registry.dispatch(UI_STORE_NAME).setStep(2);
+			registry.dispatch(UI_STORE_NAME).setContext('default');
 
-		const { queryByRole, waitForRegistry } = render(<EditForm />, {
-			registry,
+			const { queryByRole, waitForRegistry } = render(<EditForm />, {
+				registry,
+			});
+
+			await waitForRegistry();
+
+			const createUserModal = queryByRole('dialog');
+			expect(createUserModal).not.toBeInTheDocument();
 		});
 
-		await waitForRegistry();
+		it('when the current step is greater than allowd number of steps', async () => {
+			registry.dispatch(UI_STORE_NAME).setStep(3);
+			registry.dispatch(UI_STORE_NAME).setContext('edit');
 
-		const createUserModal = queryByRole('dialog');
-		expect(createUserModal).not.toBeInTheDocument();
+			const { queryByRole, waitForRegistry } = render(<EditForm />, {
+				registry,
+			});
+
+			await waitForRegistry();
+
+			const createUserModal = queryByRole('dialog');
+			expect(createUserModal).not.toBeInTheDocument();
+		});
 	});
 
-	it('should render null when the current step is greater than allowd number of steps', async () => {
-		registry.dispatch(UI_STORE_NAME).setStep(3);
-		registry.dispatch(UI_STORE_NAME).setContext('edit');
+	describe('renders the modal', () => {
+		it('when context is edit and step count is valid', async () => {
+			registry.dispatch(UI_STORE_NAME).setStep(0);
+			registry.dispatch(UI_STORE_NAME).setContext('edit');
 
-		const { queryByRole, waitForRegistry } = render(<EditForm />, {
-			registry,
+			const { getByText, getByRole, waitForRegistry } = render(
+				<EditForm />,
+				{
+					registry,
+				}
+			);
+
+			await waitForRegistry();
+
+			const createUserModal = getByRole('dialog');
+			expect(createUserModal).toBeInTheDocument();
+
+			// Check the current step is active in the form.
+			const activeStepper = getByText('User Details');
+			expect(activeStepper).toHaveClass('Mui-active');
+
+			const inActiveStepper = getByText('Access Details');
+			expect(inActiveStepper).not.toHaveClass('Mui-active');
 		});
-
-		await waitForRegistry();
-
-		const createUserModal = queryByRole('dialog');
-		expect(createUserModal).not.toBeInTheDocument();
-	});
-
-	it('should add the modal to the document', async () => {
-		registry.dispatch(UI_STORE_NAME).setStep(0);
-		registry.dispatch(UI_STORE_NAME).setContext('edit');
-
-		const { getByRole, waitForRegistry } = render(<EditForm />, {
-			registry,
-		});
-
-		await waitForRegistry();
-
-		const createUserModal = getByRole('dialog');
-		expect(createUserModal).toBeInTheDocument();
 	});
 });
